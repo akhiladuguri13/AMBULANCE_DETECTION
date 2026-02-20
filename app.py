4d313b42583efc5d5fe95cb4bf615e099bfae9fb
akhiladuguri13 <akhiladuguri13@gmail.com>
2026-02-20 23:45:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import torch
+import torch.nn as nn
+from torchvision import models, transforms
+from PIL import Image
+
+# Device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load model
+model = models.resnet18(weights=None)
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 2)
+model.load_state_dict(torch.load("ambulance_model.pth", map_location=device))
+model = model.to(device)
+model.eval()
+
+# Transform
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+])
+
+st.title("🚑 Ambulance Detection App")
+
+uploaded_file = st.file_uploader("Upload an image", type=["jpg","png","jpeg"])
+
+if uploaded_file is not None:
+    img = Image.open(uploaded_file).convert("RGB")
+    st.image(img, caption="Uploaded Image", use_column_width=True)
+
+    img_tensor = transform(img).unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        output = model(img_tensor)
+        _, predicted = torch.max(output, 1)
+
+    label_map = {0: "Non-Ambulance", 1: "Ambulance"}
+    prediction = label_map[predicted.item()]
+
+    st.subheader(f"Prediction: {prediction}")
